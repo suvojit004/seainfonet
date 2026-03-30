89fb9b4d6d92792aaf698715d86d6efcf17c38cc
suvojit004 <shuvojit.pal004@gmail.com>
2026-03-30 12:04:35 +0530

    Addition of footer and presence section

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/about', (req,res)=>{
+  res.render('about')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
