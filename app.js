633b6df461f4002270fb88ff7ec08452bf5b09c0
suvojit004 <shuvojit.pal004@gmail.com>
2026-04-02 11:05:02 +0530

    Addition of custon 404 and error handler and disabled x-powered-by header

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const multer = require("multer");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set('view engine', 'ejs');
+app.set('views', './views');
 const upload = multer();
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.disable('x-powered-by');
 
 
-const port = 3000
+
+const port = 3000;
 
 app.get('/', (req, res) => {
   res.render('index')
-})
+});
 
 app.get('/about', (req, res) => {
   res.render('about')
-})
+});
 app.get('/contact', (req, res) => {
   res.render('contact')
-})
+});
 
 app.post('/submit', upload.none(), (req, res) => {
   console.log("Form Data Received:");
   console.log(req.body);
   res.status(200).json({ message: "Form submitted successfully" });
-})
+});
 
 app.get('/demo', (req, res) => {
   res.render('demo')
-})
-
+});
 app.get('/test', (req, res) => {
   res.render('modals')
-})
+});
+
+
+// custom 404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!")
+});
+
+// custom error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
 
 
